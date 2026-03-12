23e660a22cfa459f49bfe8dab37adc583e726338
Sierra Hart <98795941+Sierrahart@users.noreply.github.com>
2026-03-11 18:39:27 -0600

    Delete pp_conformers directory

--- pp_conformers/run_crest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in *inp # submit crest submissions ONLY for .xyz files which have specified constraints in a .inp file.
-  do
-    FILE=$(basename $i .inp)
-    cp crest_bisphosphine.slurm ${FILE}_crest.slurm
-    sbatch ${FILE}_crest.slurm $FILE 0 ${UUFSCELL::-6}
-    echo $FILE >> conformers.txt
-  done
